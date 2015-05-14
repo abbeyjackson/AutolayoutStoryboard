c1ab19787faff37c2034890eff98a7fff44ba2fe
Abbey Jackson <abbey@abbeyjackson.ca>
2015-05-13 17:10:56 -0700

    Create autolayout project via storyboard

---
+++ AutolayoutStoryboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutolayoutStoryboard
+//
+//  Created by Abegael Jackson on 2015-05-13.
+//  Copyright (c) 2015 Abegael Jackson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
